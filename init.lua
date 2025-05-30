9394fe436974824d5aedbc13c34e3491227bd49f
noah-tah <noahtah21@gmail.com>
2025-05-30 16:07:23 -0500

    added transparency to see my cool background

--- init.lua
+++ init.lua
@@ -663,3 +663,9 @@ vim.o.indentexpr = '' -- Disable expression-based indenting
 --
 --
 vim.o.wrap = false
+
+vim.cmd [[
+     highlight Normal guibg=NONE ctermbg=NONE
+     highlight NonText guibg=NONE ctermbg=NONE
+     highlight EndOfBuffer guibg=NONE ctermbg=NONE
+ ]]
